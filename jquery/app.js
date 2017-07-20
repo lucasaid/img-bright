203cb49dcdb8da46bb00917519944339ce362a94
Chris Lucas <chris@hhd.com.au>
2017-07-20 17:05:25 +1000

    Added some functionality for demo stuff

--- jquery/app.js
+++ jquery/app.js
@@ -1,3 +1,18 @@
+function checkH1(){
+  var offset = $('h1').offset();
+  $('.banner').imgBright({
+    width: $('h1').outerWidth(),
+    height: $('h1').outerHeight(),
+    xCoord:offset.left,
+    yCoord:offset.top,
+    callback: function(brightness){
+      if(brightness < 140)
+        $("h1").addClass("dark");
+      else
+        $("h1").removeClass("dark");
+    }
+  });
+}
 $(document).ready(function(){
   $('.banner').imgBright({
     width:0,
@@ -9,61 +24,35 @@ $(document).ready(function(){
         $("header").addClass("dark");
     }
   });
-  $('.banner').imgBright({
-    width: $('h1').outerWidth(),
-    height:$('h1').outerHeight(),
-    xCoord:"10%",
-    yCoord:"30%",
-    callback: function(brightness){
-      if(brightness < 140)
-        $("h1").addClass("dark");
-      else
-        $("h1").removeClass("dark");
+  checkH1();
+
+  $(document).keydown(function(e){
+    if(e.which == 13) {
+      var image = "/images/"+(Math.floor((Math.random() * 5) + 1))+".jpeg";
+      $(".banner").css('background-image', "url('"+image+"')");
+
+      $('.banner').imgBright({
+        width:0,
+        height:$('header').outerHeight(),
+        xCoord:0,
+        yCoord:0,
+        callback: function(brightness){
+          if(brightness < 140)
+            $("header").addClass("dark");
+          else
+            $("header").removeClass("dark");
+        }
+      });
+
+      checkH1();
     }
   });
-
-  $('.banner').click(function(){
-    var image = "/images/"+(Math.floor((Math.random() * 5) + 1))+".jpeg";
-    $(".banner").css('background-image', "url('"+image+"')");
-
-    $('.banner').imgBright({
-      width:0,
-      height:$('header').outerHeight(),
-      xCoord:0,
-      yCoord:0,
-      callback: function(brightness){
-        if(brightness < 140)
-          $("header").addClass("dark");
-        else
-          $("header").removeClass("dark");
-      }
-    });
-
-    $('.banner').imgBright({
-      width: $('h1').outerWidth(),
-      height:$('h1').outerHeight(),
-      xCoord:"10%",
-      yCoord:"30%",
-      callback: function(brightness){
-        if(brightness < 140)
-          $("h1").addClass("dark");
-        else
-          $("h1").removeClass("dark");
-      }
-    });
+  $(document).click(function(e){
+    $("h1").css('left',e.clientX);
+    $("h1").css('top',e.clientY);
+    checkH1();
   });
   $(window).resize(function(){
-    $('.banner').imgBright({
-      width: $('h1').outerWidth(),
-      height:$('h1').outerHeight(),
-      xCoord:"10%",
-      yCoord:"30%",
-      callback: function(brightness){
-        if(brightness < 140)
-          $("h1").addClass("dark");
-        else
-          $("h1").removeClass("dark");
-      }
-    });
+    checkH1();
   })
 })
